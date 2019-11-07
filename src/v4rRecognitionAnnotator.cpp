7de3956836c6d0e3697af6fee9d6a06c4a208edc
Diplomarbeit1 <ChristophBerger1@gmx.at>
2019-11-07 16:57:30 +0100

    basic implementation for v4r recognition annotator

--- src/v4rRecognitionAnnotator.cpp
+++ src/v4rRecognitionAnnotator.cpp
@@ -29,18 +29,37 @@
 #include <pcl-1.7/pcl/io/pcd_io.h>
 #include <opencv2/highgui/highgui.hpp>
 
+// ppf service definitions
+#include "ppf_recognizer_ros_msgs/recognize.h"
+// #include "ppf_recognizer_ros_msgs/set_camera.h"
+
+#include "pcl_conversions/pcl_conversions.h"
+
 
 using namespace uima;
 
 class v4rRecognitionAnnotator : public Annotator {
 private:
+  ros::NodeHandle nh_;
+  ros::ServiceClient recognition_client_;
+  // ros::ServiceClient set_camera_client_;
 
 
 public:
 
     TyErrorId initialize(AnnotatorContext &ctx) {
       outInfo("initialize");
-        return UIMA_ERR_NONE;
+      // initialize ros service clients
+      std::map<std::string, std::string> dummy_args;
+      // ros::init(dummy_args, "ppf annotator");
+      // nh_.reset(new ros::NodeHandle("~"));
+
+      recognition_client_ = nh_.serviceClient<ppf_recognizer_ros_msgs::recognize>("/object_recognition/recognize");
+      ros::Publisher pub = nh_.advertise<std_msgs::String>("test_topic_name", 5);
+
+     
+
+      return UIMA_ERR_NONE;
     }
 
     TyErrorId destroy() {
@@ -51,6 +70,8 @@ public:
     TyErrorId process(CAS &tcas, ResultSpecification const &res_spec) {
       outInfo("processing");
 
+      ROS_INFO("v4rRecognitionAnnotator");
+      
       rs::SceneCas cas(tcas);
       pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGBA>);
       rs::StopWatch clock;
@@ -70,9 +91,29 @@ public:
           pcl::ExtractIndices < pcl::PointXYZRGBA > ei;
           ei.setInputCloud(cloud_ptr);
           ei.setIndices(indices);
+          ei.setKeepOrganized(true);
           ei.filter(*cluster_cloud);
 
           //TODO ->make msg, send server goal, recieve msgs -> annotate
+
+          // create srv request
+          ppf_recognizer_ros_msgs::recognize srv;
+
+          sensor_msgs::PointCloud2 msg_cloud;
+          pcl::toROSMsg(*cluster_cloud, msg_cloud);
+
+          srv.request.cloud = msg_cloud; 
+
+          
+          std_msgs::String object_id;
+          geometry_msgs::Transform object_transform;
+
+          // call ros service with pointcloud
+          if (recognition_client_.call(srv)) {
+            object_id = srv.response.ids.front();
+            object_transform = srv.response.transforms.front();
+            outInfo(object_id.data);
+          }
 
         }
       }
